7eeac1f0e39ba191375fe861ab05e9a8935e3f21
kazuyuki <m.kazuyuki@gmail.com>
2023-09-14 00:43:50 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+self.addEventListener("install", event => {
+  console.log('WORKER: Install', event.request);
+});
+self.addEventListener("activate", event => {
+  console.log('WORKER: Activate', event.request);
+});
+self.addEventListener("fetch", event => {
+  console.log('WORKER: Fetch', event.request);
+});
